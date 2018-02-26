df37fc21c669ec44ed519dcdb2d518c4894e0d43
319ganesh <31438442+319ganesh@users.noreply.github.com>
2018-02-26 03:09:11 -0800

    Create median.c

---
+++ median.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+int a[10],n,i,j,c;
+printf("\n enter the value of n:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("\n enter the value:");
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n-i-1;j++)
+{
+if(a[j]>a[j+1])
+{
+c=a[j];
+a[j]=a[j+1];
+a[j+1]=c;
+}
+}
+}
+
+printf("\n the shorted array is:%d",a[n/2]);
+
+return 0;
+}
